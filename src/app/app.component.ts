e458db283b8d998be2b3494d99b54a6347114fbc
Dirk Bunk <dirk.bunk@lhind.dlh.de>
2025-05-22 11:00:15 +0200

    added start component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal, untracked } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { DecimalPipe } from '@angular/common';
 
 interface Depot {
   name: string;
@@ -11,15 +13,30 @@ interface Depot {
   shareFactor: number;
 }
 
+interface Item {
+  name: string;
+  amount: number;
+  rest: number;
+  shares: Share[];
+}
+
+interface Share {
+  depot: Depot;
+  isDepotActive: boolean;
+  amountPerHalfShare: number;
+  amountPerPerson: number;
+}
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatTableModule],
+  imports: [RouterOutlet, MatTableModule, MatInputModule, DecimalPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  depots: Depot[] = [];
-  displayedColumns: string[] = ['depot', 'half-shares', 'full-shares', 'persons'];
+  depots = signal<Depot[]>([]);
+  items = signal<Item[]>([]);
+  displayedColumns: string[] = ['depot', 'half-shares', 'full-shares', 'persons', 'per-half-share-amount', 'per-person-amount'];
 
   constructor() {
     this.addDepot('Wiesbaden', 6, 2);
@@ -27,25 +44,57 @@ export class AppComponent {
     this.addDepot('Nauheim', 7, 3);
     this.addDepot('Raunheim', 8, 2);
 
-    this.depots.forEach(depot => this.calculateShareFactor(depot));
+    this.depots().forEach(depot => this.calculateShareFactor(depot));
+
+    this.addItem('Kohlrabi', 500);
+
+    effect(() => {
+      const items = this.items();
+
+      untracked(() => this.calculateShares(items));
+    });
   }
 
   private addDepot(name: string, halfShares: number, fullShares: number) {
-    this.depots.push({
+    this.depots.set([...this.depots(), {
       name: name,
       halfShares: halfShares,
       fullShares: fullShares,
       shares: halfShares + fullShares * 2,
       persons: halfShares + fullShares,
       shareFactor: 1
-    });
+    }]);
   }
 
   private calculateShareFactor(depot: Depot) {
-    const totalShares = this.depots.reduce((previousValue, currentValue) => {
+    const totalShares = this.depots().reduce((previousValue, currentValue) => {
       return previousValue + currentValue.shares;
     }, 0);
 
     depot.shareFactor = depot.shares / totalShares;
+  }
+
+  private addItem(name: string, amount: number) {
+    this.items.set([...this.items(), {
+      name,
+      amount,
+      rest: amount,
+      shares: this.depots().map(depot => {
+        return {
+          depot: {...depot},
+          isDepotActive: true,
+          amountPerHalfShare: 0,
+          amountPerPerson: 0
+        }
+      })
+    }]);
+  }
+
+  private calculateShares(items: Item[]) {
+    items.forEach(item => {
+      item.shares.forEach(share => {
+        share.amountPerHalfShare = share.depot.shareFactor * item.amount;
+      });
+    });
   }
 }
